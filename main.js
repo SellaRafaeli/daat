21a6123025a886378e5b297eb797de02b1789e8e
Sella Rafaeli <sella.r@fiverr.com>
2014-01-28 20:06:39 +0200

    first draft

---
+++ main.js
@@ -0,0 +1,41 @@
+var myApp = angular.module('myApp', []);
+
+id = 0;
+getQ = function(title,body,answers) {
+    id = id+1;
+    return {id: id, title: title, body: body, answers:answers}
+}
+
+getA = function(user,body){
+    return {user: user, body: body};
+}
+
+myApp.factory('Data', function() {
+    return {
+        message: 'new data from a service',
+        qList: [
+            getQ('What time is it?','Is it 16:20?',[getA("Bob",'it\'s 16:15'), getA("Yossi",' it is 10:30')]),
+            getQ('Which university is the best?', 'Technion? Hebrew U?', 
+                [getA("Yossi",'I don\'t know where to go to school'),
+                 getA("Moshe",'Hebrew University'),
+                 getA("Haim",'Technion')]),
+            getQ('Who do you think should be president?', 'Dalya Itzik? Shechtman?', 
+                [getA("Moshe",'I think it should be Fuad'),
+                getA("Haim",'Yair Lapid!')])            
+        ]
+        
+    };
+});
+
+function FirstCtrl($scope, Data) {      
+    $scope.data = Data;
+}
+
+
+function qListCtrl($scope, Data){
+    $scope.data = Data; 
+    $scope.foo = 'bar'; 
+    $scope.answers = function(q){
+        q.answers;
+    }
+}
