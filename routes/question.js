19e287ea652d8d971b91ef81486be03e409b95d9
ohad partuck <ohadpartuck@gmail.com>
2014-02-01 20:37:15 +0200

    moving logic to a sepearate file

--- routes/question.js
+++ routes/question.js
@@ -1,34 +1,22 @@
+var questions = require('../lib/questions/questions.js');
 
 //NEW Question
 //Get Question by ID
 //Get Question By Category
 
 exports.list = function(req, res){
-    res.json({
-        1: "first question",
-        2: "second question",
-        3: "third question",
-        4: "forth question"
-    });
+    res.json(questions.list_top_rates(req.params));
 };
 
 exports.get = function(req, res){
-
-    res.json({
-        1: "first question"
-    });
+    res.json(questions.fetch_question(req.params.id));
 };
 
 exports.get_cat = function(req, res){
     var category = req.params.cat;
-    res.json({
-        1: "first question in category " + category,
-        2: "second question in category " + category,
-        3: "third question in category " + category,
-        4: "forth question in category " + category
-    });
+    res.json(questions.fetch_question_by_category(category));
 };
 
 exports.new_question = function(req, res){
-    res.json({result: "I have just created a new question (Yeah, right).", error: undefined});
+    res.json(questions.new_question(req.params));
 };
