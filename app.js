6d78d4288c31110a405d24f039c885c2a02e4f0d
ohad partuck <ohadpartuck@gmail.com>
2014-02-05 22:47:23 +0200

    orm works on root url

--- app.js
+++ app.js
@@ -2,17 +2,44 @@
 /**
  * Module dependencies.
  */
+var log = function(s){console.log(s);};
 
-var express     = require('express');
-var routes      = require('./routes');
-var user        = require('./routes/user');
-var question    = require('./routes/question');
-var answer      = require('./routes/answer');
-var http        = require('http');
-var path        = require('path');
+var env                = (process.env.NODE_ENV || 'DEVELOPMENT').toLowerCase();
 
-var app = express();
+var express             = require('express'),
+    routes              = require('./routes'),
+    user                = require('./routes/user_api'),
+    question            = require('./routes/question_api'),
+    answer              = require('./routes/answer_api'),
+    http                = require('http'),
+    path                = require('path');
 
+//var cfg                 = require('./configuration/' + env + '.json');
+
+var app                 = express();
+
+var orm = require('orm');
+
+app.use(orm.express("mysql://root@127.0.0.1/daat", {
+    define: function (db, models, next) {
+        var question_model =  db.define('questions', {
+            id              : Number,
+            title           : String,
+            text            : String,
+            category_id     : Number,
+            sub_category_id : Number,
+            rating          : Number
+        }, {
+            methods : {
+                fullName: function () {
+                    return this.title;
+                }
+            }
+        });
+        models.question = question_model;
+        next();
+    }
+}));
 
 
 // all environments
@@ -27,12 +54,26 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//TODO - add localization
+//TODO - load environment config file
+//TODO - add stub data(to be selected by the global config file).
+
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+
+//homepage
+//app.get('/', routes.index);
+app.get("/", function (req, res) {
+    result = req.models.question.find({id: 4}, function(err, question) {
+        res.json(question[0]);
+    });
+});
+
 app.get('/users', user.list);
 
 app.get('/questions', question.list);
@@ -47,6 +88,8 @@ app.get('/answers/question/:question', answer.get_question);
 app.get('/answers/new/:name', answer.new_answer);
 
 
+
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server on port ' + app.get('port') + ' Ready to Rock!');
+//  console.log('configurations are ' + cfg);
 });
