6a66f7d2e957fb40ed191d1eb0a11978a2cbe255
Sella Rafaeli <sella.r@fiverr.com>
2014-02-05 19:27:42 +0200

    css improvements

--- public/ui/directives_attributes.js
+++ public/ui/directives_attributes.js
@@ -1,15 +1,15 @@
 //todo: should just be part of page CSS once we remove 'foundation'
-myApp.directive('heb', function() {
-    return function (scope, element, attr) {
-        element.addClass('heb')
-    }   
-});
+// myApp.directive('heb', function() {
+//     return function (scope, element, attr) {
+//         element.addClass('heb')
+//     }   
+// });
 
-myApp.directive('show', function(){
-    return {
-        restrict: 'A',
-        link: function(scope, el, attrs){
-            if (attrs.show) { el[0].style.display = 'none'}
-        }
-    }                   
-});
+// myApp.directive('show', function(){
+//     return {
+//         restrict: 'A',
+//         link: function(scope, el, attrs){
+//             if (attrs.show) { el[0].style.display = 'none'}
+//         }
+//     }                   
+// });
