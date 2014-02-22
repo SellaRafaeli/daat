bc5dd5763627e389dde1dd2dc98b2ae83ac41834
ohad partuck <ohadpartuck@gmail.com>
2014-02-22 16:18:42 +0200

    choosing a i18 module

--- app.js
+++ app.js
@@ -19,12 +19,13 @@ var env                = (process.env.NODE_ENV || 'DEVELOPMENT').toLowerCase();
 console.log(GLOBAL.ROOT + 'in' + env);
 
 var express             = require('express'),
-    routes              = require('./routes'),
     $                   = require('jquery'),
+    routes              = require('./routes'),
     user                = require('./routes/user_api'),
     question            = require('./routes/question_api'),
     answer              = require('./routes/answer_api'),
     category            = require('./routes/category_api'),
+    i18n                = require('i18n-abide'),
     http                = require('http'),
     path                = require('path');
 
@@ -48,6 +49,11 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use(i18n.abide({
+//    supported_languages: ['en-US', 'he'],
+//    default_lang: 'he',
+//    translation_directory: 'static/i18n'
+//}));
 
 
 // development only
@@ -66,7 +72,7 @@ app.get('/questions', question.list);
 app.get('/questions/:id', question.get);
 app.get('/questions/category/:category_id', question.category);
 app.post('/questions/new/:fake_param', question.new_questions);
-app.get('/questions/edit/:id', question.edit);
+//app.get('/questions/edit/:id', question.edit);
 app.post('/questions/:id/update', question.update);
 
 app.post('/questions/:id/new_answer', answer.new);
